a2e16ecf522653ffb96d53ff9c26e7aab53ba6ce
MihirKumar12 <87576993+MihirKumar12@users.noreply.github.com>
2022-02-11 12:08:54 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://mihir:Sonu1111+@cluster0.wilvd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+
+bot_token:'5268008527:AAHs7W8M_sqcfhsv1iLwiMCyPyFMgMCjZhY',//bot token
+
+bot_name:'BNBAirdropR2_Bot',//bot username without @
+
+bot_admin:938452476,// admin telegram id
+
+channelsList : ['@AirdropV_Official','@AirdropVSupport','@CryptoFort_News','@CryptoFortChat','@AirdropPeacock','@AirdropMeteorite'],// channels list
+
+reffer_bonus:'0.00001',// refferal bonus amount
+
+min_wd:'0.00005',//min withdrawal amount
+
+daily_bonus:'1',// daily bonus amount
+
+currency:'BNB', //bot cuurency
+
+cb_api_key:'sd',//coinbase api key
+
+cb_api_secret:'sd',// coinbase secret key
+
+cb_account_id:'sd',// counbase account id for the bot currency
+
+payment_channel:'@bnbbotwithdrawall',// payment channel username
+
+}
